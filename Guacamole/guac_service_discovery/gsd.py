d78b76cd609d2a47f0668cb627feab53b4228202
BoeJaker <j.baker.cwp@gmail.com>
2024-04-02 22:02:01 +0100

    updates

--- Guacamole/guac_service_discovery/gsd.py
+++ Guacamole/guac_service_discovery/gsd.py
@@ -1,67 +1,8 @@
-# import docker
-# import requests
-
-# # Initialize Docker client
-# docker_client = docker.from_env()
-
-# # Guacamole API endpoint
-# GUACAMOLE_API_URL = 'http://192.168.3.201:8080/guacamole/api'
-
-# # Guacamole credentials
-# GUACAMOLE_USERNAME = 'guacadmin'
-# GUACAMOLE_PASSWORD = 'guacadmin'
-
-# # Function to authenticate with Guacamole API
-# def authenticate_guacamole():
-#     auth_response = requests.post(
-#         f"{GUACAMOLE_API_URL}/tokens",
-#         data={'username': GUACAMOLE_USERNAME, 'password': GUACAMOLE_PASSWORD}
-#     )
-#     auth_response.raise_for_status()
-#     return auth_response.json()['authToken']
-
-# # Function to add Docker hosts to Guacamole
-# def add_docker_hosts_to_guacamole():
-#     auth_token = authenticate_guacamole()
-#     print(auth_token)
-#     headers = {'Authorization': f'Bearer {auth_token}'}
-
-#     for container in docker_client.containers.list():
-#         # Extract container details
-#         container_name = container.name
-#         container_ip = container.attrs['NetworkSettings']['IPAddress']
-        
-#         # Create connection
-#         connection_data = {
-#             "name": container_name,
-#             "protocol": "ssh",
-#             "parameters": {
-#                 "hostname": container_ip,
-#                 "port": 22,
-#                 "username": "your_ssh_username",  # Change as needed
-#                 "password": "your_ssh_password"   # Change as needed
-#             }
-#         }
-
-#         # Add connection to Guacamole
-#         connection_response = requests.post(
-#             f"{GUACAMOLE_API_URL}/session/data/postgresql/connections",
-#             json=connection_data,
-#             headers=headers
-#         )
-#         connection_response.raise_for_status()
-
-#         print(f"Added container {container_name} with IP {container_ip} to Guacamole.")
-
-# # Execute function to add Docker hosts to Guacamole
-# add_docker_hosts_to_guacamole()
-
-
 import psycopg2
 import docker
 # from dotenv import dotenv_values
 
-password="xxxxxxxx" #dotenv_values("~/BACKUPS/Master/Dev/Docker_Containers/Production/Docker-Red-Lab/Guacamole/guac_service_discovery/.env")
+password=dotenv_values("~/BACKUPS/Master/Dev/Docker_Containers/Production/Docker-Red-Lab/Guacamole/guac_service_discovery/.env")
     
 # Function to connect to the database
 def connect_to_database():
